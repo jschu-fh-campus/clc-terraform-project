b6a4f2ce57e151bca5c29e84c1ba6de421745e0c
Johannes Schumeth <johannes.schumeth@gmail.com>
2021-01-09 18:15:58 +0100

    create notifier and dashboard for grafana

--- userdata/monitoring.sh
+++ userdata/monitoring.sh
@@ -70,7 +70,7 @@ docker run \
   --net=monitoring \
   jschu/exoscale_service_discovery
 
-# create prometheus datasource for grafana on instance
+# create datasource for grafana on instance
 echo """
 apiVersion: 1
 datasources:
@@ -83,11 +83,230 @@ datasources:
   editable: false
 """ >> /srv/datasource.yml
 
+# create notifier for grafana on instance
+echo """
+notifiers:
+  - name: Scale up
+    type: webhook
+    uid: scale-up
+    org_id: 1
+    is_default: false
+    send_reminder: true
+    disable_resolve_message: true
+    frequency: '2m'
+    settings:
+      autoResolve: true
+      httpMethod: 'POST'
+      severity: 'critical'
+      uploadImage: false
+      url: 'http://autoscaler:8090/up'
+""" >> /srv/notifier.yml
+
+# create dashboard configuration for grafana on instance
+echo """
+apiVersion: 1
+providers:
+- name: 'Home'
+  orgId: 1
+  folder: ''
+  type: file
+  updateIntervalSeconds: 10
+  options:
+    path: /etc/grafana/dashboards
+""" >> /srv/dashboard.yml
+
+# create dashboard for grafana on instance
+echo """
+{
+  \"annotations\": {
+    \"list\": [
+      {
+        \"builtIn\": 1,
+        \"datasource\": \"-- Grafana --\",
+        \"enable\": true,
+        \"hide\": true,
+        \"iconColor\": \"rgba(0, 211, 255, 1)\",
+        \"name\": \"Annotations & Alerts\",
+        \"type\": \"dashboard\"
+      }
+    ]
+  },
+  \"editable\": true,
+  \"gnetId\": null,
+  \"graphTooltip\": 0,
+  \"id\": 1,
+  \"links\": [],
+  \"panels\": [
+    {
+      \"alert\": {
+        \"alertRuleTags\": {},
+        \"conditions\": [
+          {
+            \"evaluator\": {
+              \"params\": [
+                0.8
+              ],
+              \"type\": \"gt\"
+            },
+            \"operator\": {
+              \"type\": \"and\"
+            },
+            \"query\": {
+              \"params\": [
+                \"A\",
+                \"5m\",
+                \"now\"
+              ]
+            },
+            \"reducer\": {
+              \"params\": [],
+              \"type\": \"avg\"
+            },
+            \"type\": \"query\"
+          }
+        ],
+        \"executionErrorState\": \"alerting\",
+        \"for\": \"5m\",
+        \"frequency\": \"1m\",
+        \"handler\": 1,
+        \"name\": \"CPU usage\",
+        \"noDataState\": \"no_data\",
+        \"notifications\": [
+          {
+            \"uid\": \"scale-up\"
+          }
+        ]
+      },
+      \"aliasColors\": {},
+      \"bars\": false,
+      \"dashLength\": 10,
+      \"dashes\": false,
+      \"datasource\": \"Prometheus\",
+      \"fieldConfig\": {
+        \"defaults\": {
+          \"custom\": {}
+        },
+        \"overrides\": []
+      },
+      \"fill\": 1,
+      \"fillGradient\": 0,
+      \"gridPos\": {
+        \"h\": 9,
+        \"w\": 12,
+        \"x\": 0,
+        \"y\": 0
+      },
+      \"hiddenSeries\": false,
+      \"id\": 2,
+      \"legend\": {
+        \"avg\": false,
+        \"current\": false,
+        \"max\": false,
+        \"min\": false,
+        \"show\": true,
+        \"total\": false,
+        \"values\": false
+      },
+      \"lines\": true,
+      \"linewidth\": 1,
+      \"nullPointMode\": \"null\",
+      \"options\": {
+        \"alertThreshold\": true
+      },
+      \"percentage\": false,
+      \"pluginVersion\": \"7.3.6\",
+      \"pointradius\": 2,
+      \"points\": false,
+      \"renderer\": \"flot\",
+      \"seriesOverrides\": [],
+      \"spaceLength\": 10,
+      \"stack\": false,
+      \"steppedLine\": false,
+      \"targets\": [
+        {
+          \"expr\": \"sum by (instance) (rate(node_cpu_seconds_total{mode!=\\"idle\\"}[1m])) / sum by (instance) (rate(node_cpu_seconds_total[1m]))\",
+          \"interval\": \"\",
+          \"legendFormat\": \"\",
+          \"queryType\": \"randomWalk\",
+          \"refId\": \"A\"
+        }
+      ],
+      \"thresholds\": [
+        {
+          \"colorMode\": \"critical\",
+          \"fill\": true,
+          \"line\": true,
+          \"op\": \"gt\",
+          \"value\": 0.8
+        }
+      ],
+      \"timeFrom\": null,
+      \"timeRegions\": [],
+      \"timeShift\": null,
+      \"title\": \"Panel Title\",
+      \"tooltip\": {
+        \"shared\": true,
+        \"sort\": 0,
+        \"value_type\": \"individual\"
+      },
+      \"type\": \"graph\",
+      \"xaxis\": {
+        \"buckets\": null,
+        \"mode\": \"time\",
+        \"name\": null,
+        \"show\": true,
+        \"values\": []
+      },
+      \"yaxes\": [
+        {
+          \"format\": \"short\",
+          \"label\": null,
+          \"logBase\": 1,
+          \"max\": null,
+          \"min\": null,
+          \"show\": true
+        },
+        {
+          \"format\": \"short\",
+          \"label\": null,
+          \"logBase\": 1,
+          \"max\": null,
+          \"min\": null,
+          \"show\": true
+        }
+      ],
+      \"yaxis\": {
+        \"align\": false,
+        \"alignLevel\": null
+      }
+    }
+  ],
+  \"schemaVersion\": 26,
+  \"style\": \"dark\",
+  \"tags\": [],
+  \"templating\": {
+    \"list\": []
+  },
+  \"time\": {
+    \"from\": \"now-6h\",
+    \"to\": \"now\"
+  },
+  \"timepicker\": {},
+  \"timezone\": \"\",
+  \"title\": \"CPU usage\",
+  \"uid\": \"2urSUP-Gk\",
+  \"version\": 2
+}
+""" >> /srv/dashboard.json
+
 #Run Grafana
 docker run \
   -d \
   -p 3000:3000 \
   -v /srv/datasource.yml:/etc/grafana/provisioning/datasources/datasource.yml \
+  -v /srv/notifier.yml:/etc/grafana/provisioning/notifiers/notifier.yml \
+  -v /srv/dashboard.yml:/etc/grafana/provisioning/dashboards/dashboard.yml \
+  -v /srv/dashboard.json:/etc/grafana/dashboards/dashboard.json \
   --name grafana \
   --net=monitoring \
   grafana/grafana
